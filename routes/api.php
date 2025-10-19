2f14c54e520f085759efd7a4c58c65c3bf06fb8e
putra-pkwl03 <putrapongkowulu@gmail.com>
2025-10-20 03:09:50 +0700

    menamabhkan api refresh token dan manage form

--- routes/api.php
+++ routes/api.php
@@ -8,6 +8,7 @@ use App\Http\Controllers\DosenProfileController;
 
 Route::post('/register', [AuthController::class, 'register']); 
 Route::post('/auth/login', [AuthController::class, 'login']);
+Route::post('/auth/refresh', [AuthController::class, 'refresh']);
 Route::post('/auth/logout', [AuthController::class, 'logout']);
 
 Route::middleware(['auth:api', 'role:admin'])->group(function () {
