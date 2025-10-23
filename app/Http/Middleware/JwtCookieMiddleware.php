92fa0757b7b7517999f313fbb3abce472f08f4d6
putra-pkwl03 <putrapongkowulu@gmail.com>
2025-10-23 09:48:47 +0700

    membuat midleware cookie costume

--- app/Http/Middleware/JwtCookieMiddleware.php
+++ app/Http/Middleware/JwtCookieMiddleware.php
@@ -11,22 +11,17 @@ class JwtCookieMiddleware
     public function handle($request, Closure $next)
     {
         try {
-            // Ambil token dari cookie
             $token = $request->cookie('pcys');
 
             if (!$token) {
                 return response()->json(['message' => 'Token cookie missing'], 401);
             }
-
-            // Set token ke JWTAuth dan autentikasi user
             JWTAuth::setToken($token);
             $user = JWTAuth::authenticate();
 
             if (!$user) {
                 return response()->json(['message' => 'User not found'], 401);
             }
-
-            // Set user ke request agar bisa dipakai di controller
             $request->merge(['auth_user' => $user]);
         } catch (Exception $e) {
             return response()->json(['message' => 'Unauthenticated', 'error' => $e->getMessage()], 401);
