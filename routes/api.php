5855c1192e6d63648cab71b3b94502f3a072c325
putra-pkwl03 <putrapongkowulu@gmail.com>
2025-10-23 09:46:43 +0700

    menyesuikan auth api routs

--- routes/api.php
+++ routes/api.php
@@ -11,19 +11,19 @@ Route::post('/auth/login', [AuthController::class, 'login']);
 Route::post('/auth/refresh', [AuthController::class, 'refresh']);
 Route::post('/auth/logout', [AuthController::class, 'logout']);
 
-Route::middleware(['auth:api', 'role:admin'])->group(function () {
+Route::middleware(['jwt.cookie', 'role:admin'])->group(function () {
     Route::get('/admin', [AuthController::class, 'index']);
     Route::get('/admin/{id}', [AuthController::class, 'show']);
 });
 
-Route::middleware(['auth:api', 'role:mahasiswa'])->group(function () {
+Route::middleware(['jwt.cookie', 'role:mahasiswa'])->group(function () {
     Route::get('/mahasiswa', [MahasiswaProfileController::class, 'index']);
     Route::get('/mahasiswa/{id}', [MahasiswaProfileController::class, 'show']);
     Route::put('/mahasiswa/{id}', [MahasiswaProfileController::class, 'update']);
     Route::delete('/mahasiswa/{id}', [MahasiswaProfileController::class, 'destroy']);
 });
 
-Route::middleware(['auth:api', 'role:dosen'])->group(function () {
+Route::middleware(['jwt.cookie', 'role:dosen'])->group(function () {
     Route::get('/dosen', [DosenProfileController::class, 'index']);
     Route::get('/dosen/{id}', [DosenProfileController::class, 'show']);
     Route::put('/dosen/{id}', [DosenProfileController::class, 'update']);
