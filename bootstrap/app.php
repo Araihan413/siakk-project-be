45d57a2cd6e4110964779f27d1689d00d6832793
putra-pkwl03 <putrapongkowulu@gmail.com>
2025-10-23 09:46:18 +0700

    menambahkan config cookie mdr

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,6 +14,7 @@ return Application::configure(basePath: dirname(__DIR__))
         ->withMiddleware(function (Middleware $middleware): void {
             $middleware->alias([
                 'role' => \App\Http\Middleware\RoleMiddleware::class,
+                'jwt.cookie' => \App\Http\Middleware\JwtCookieMiddleware::class,
             ]);
 
             $middleware->append(\Illuminate\Http\Middleware\HandleCors::class);
